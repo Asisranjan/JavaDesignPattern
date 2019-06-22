e43b55b1660ca1f9ebbf10f84836d708e815dc42
Asisranjan <36892646+Asisranjan@users.noreply.github.com>
2019-06-22 09:38:22 +0530

    Add files via upload

---
+++ src/com/example/designpattern/ch07/adaptive/DuckAdapter.java
@@ -0,0 +1,23 @@
+package com.example.designpattern.ch07.adaptive;
+
+public class DuckAdapter implements Turkey {
+	Duck duck;
+	
+	public DuckAdapter(Duck duck) {
+		// TODO Auto-generated constructor stub
+		this.duck = duck;
+	}
+	
+	@Override
+	public void gobble() {
+		// TODO Auto-generated method stub
+		duck.quack();
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		duck.fly();
+	}
+
+}
